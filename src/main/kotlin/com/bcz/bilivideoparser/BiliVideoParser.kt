0d1c8fe8028e9d6d40392ff21dfc1c566bbbe6c9
BestBcz <kymandu@163.com>
2025-08-17 15:33:30 +0800

    feat:添加了视频动态的解析#14

--- src/main/kotlin/com/bcz/bilivideoparser/BiliVideoParser.kt
+++ src/main/kotlin/com/bcz/bilivideoparser/BiliVideoParser.kt
@@ -30,7 +30,7 @@ object BiliVideoParser : KotlinPlugin(
     JvmPluginDescription(
         id = "com.bcz.bilivideoparser",
         name = "BiliVideoParser",
-        version = "1.2.1"
+        version = "1.2.2"
         //https://github.com/BestBcz/BiliURL
     ) {
         author("Bcz")
